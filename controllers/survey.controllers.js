7c56e054f4cbe0f1ded8b58e74cdc05c207f5031
Omar Sh. Dieh <omardieh@github.comm>
2022-08-21 06:35:02 +0200

    database resp of survey by id req

--- controllers/survey.controllers.js
+++ controllers/survey.controllers.js
@@ -1,16 +1,27 @@
 const db = require("../db");
 
 exports.getSurveyById = async (req, res, next) => {
-  const { id } = req.query;
+  const { id } = req.params;
 
   try {
-    db.query("SELECT * FROM `survey` WHERE id='" + id + "'", (err, result) => {
-      if (err) {
-        throw err;
-      } else {
-        next(res.send(result));
+    db.query(
+      "SELECT DISTINCT survey.label as survey_label, question_type.label as question_type, question.question_content as question_content, possible_answer.content as possible_answer FROM survey JOIN question_type JOIN question JOIN possible_answer WHERE survey.id='" +
+        id +
+        "'",
+      (err, result) => {
+        if (err) {
+          throw err;
+        } else {
+          next(
+            res.send([
+              ...new Map(
+                result.map((item) => [item["question_type"], item])
+              ).values(),
+            ])
+          );
+        }
       }
-    });
+    );
   } catch (err) {
     console.log(err);
   }
