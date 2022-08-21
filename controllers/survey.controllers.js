abf28deb5028363b51319e38b4685305cbe39172
Omar Sh. Dieh <omardieh@github.comm>
2022-08-21 21:58:26 +0200

    updated data relations

--- controllers/survey.controllers.js
+++ controllers/survey.controllers.js
@@ -5,7 +5,7 @@ exports.getSurveyById = async (req, res, next) => {
 
   try {
     db.query(
-      "SELECT DISTINCT survey.label as survey_label, question_type.label as question_type, question.question_content as question_content, possible_answer.content as possible_answer FROM survey JOIN question_type JOIN question JOIN possible_answer WHERE survey.id='" +
+      "SELECT DISTINCT survey.label as survey_label, question_type.label as question_type, question.question_content as question_content, question.id as question_id, possible_answer.content as possible_answer FROM survey JOIN question_type JOIN question JOIN possible_answer WHERE survey.id='" +
         id +
         "'",
       (err, result) => {
@@ -15,7 +15,7 @@ exports.getSurveyById = async (req, res, next) => {
           next(
             res.send([
               ...new Map(
-                result.map((item) => [item["question_type"], item])
+                result.map((item) => [item["question_id"], item])
               ).values(),
             ])
           );
